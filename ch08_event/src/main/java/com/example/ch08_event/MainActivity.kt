e107d65ae4eb37f78078a514f98c22cb311e4721
zlohp <jiae4042@naver.com>
2025-04-08 03:06:25 +0900

    ch08 이벤트 실습 및 과제

--- ch08_event/src/main/java/com/example/ch08_event/MainActivity.kt
+++ ch08_event/src/main/java/com/example/ch08_event/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.ch08_event
 
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ch08_event.databinding.ActivityMainBinding
 
@@ -13,9 +16,47 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.chbStart.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked == true){
+                binding.choiceAnimal.visibility = View.VISIBLE
+            }
+            else{
+                binding.choiceAnimal.visibility = View.INVISIBLE
+            }
+        }
+
+        binding.btnOk.setOnClickListener {
+            if(binding.rdDog.isChecked){
+                binding.ivAnimal.setImageResource(R.drawable.dog)
+            }
+            else if(binding.rdCat.isChecked){
+                binding.ivAnimal.setImageResource(R.drawable.cat)
+            }
+            else if(binding.rdRabbit.isChecked){
+                binding.ivAnimal.setImageResource(R.drawable.rabbit)
+            }
+        }
+
+        binding.ivAnimal.setOnLongClickListener{
+            //Log.d("tag", "ImageView LONG click event")
+
+            Toast.makeText(this,"ImageView LONG click event",Toast.LENGTH_LONG).show()
+            //val my_toast = Toast.makeText(this, "ImageView LONG click event", Toast.LENGTH_LONG)
+            //my_toast.show()
+            true
+        }
+
+        binding.rdGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.rd_dog -> {Toast.makeText(this,"Dog check",Toast.LENGTH_SHORT).show()}
+                R.id.rd_cat -> {Toast.makeText(this,"Cat check",Toast.LENGTH_SHORT).show()}
+                R.id.rd_rabbit -> {Toast.makeText(this,"Rabbit check",Toast.LENGTH_SHORT).show()}
+            }
+        }
+
     }
 
-    //터치이벤트
+    //터치이벤트 콜백함수
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
         when(event?.action){
@@ -23,5 +64,22 @@ class MainActivity : AppCompatActivity() {
             MotionEvent.ACTION_UP -> Log.d("tag", "Touch Up Event")
         }
         return super.onTouchEvent(event)
+    }
+
+    //키이벤트 콜백함수
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("tag", "onKeyDown")
+        when(keyCode){
+            KeyEvent.KEYCODE_A -> {Log.d("tag", "onKeyDown : A")}
+            KeyEvent.KEYCODE_1 -> {Log.d("tag", "onKeyDown : 1")}
+            KeyEvent.KEYCODE_BACK -> {Log.d("tag", "onKeyDown : BACK")}
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("tag", "onKeyUp")
+        return super.onKeyUp(keyCode, event)
     }
 }
